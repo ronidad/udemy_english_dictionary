55f3a968f653e76437abce0aeca906e9134a4339
Ronald Mosomi <ronaldmosomi@gmail.com>
2021-08-26 22:56:57 +0300

    add excel files wwith different sheets

--- priactice.py
+++ priactice.py
@@ -1,5 +1,17 @@
 import pandas as pd
 
-data = pd.read_excel("sheets.xlsx", sheet_name=0)
+data1 = pd.read_excel("sheets.xlsx", sheet_name=0)
 
-print(data)
+data2 = pd.read_excel("sheets.xlsx", sheet_name=1)
+print("Excel file 1, sheet 1")
+print(data1)
+print("Exele fine 1, sheet 2")
+print(data2)
+
+
+#reading txt files. we use csv read method but those separated by ; we use sep=";"
+
+data3 = pd.read_csv("supermarkets-semi-colons.txt", sep=";")
+
+print("Supermarket file data")
+print(data3)
